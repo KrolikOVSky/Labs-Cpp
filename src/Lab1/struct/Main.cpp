acfcc459c7496d7378b7db3418a864978eb184fb
SpiritWinner <spirit.no.win@gmail.com>
2020-10-29 23:24:00 +0300

    ---

--- src/Lab1/struct/Main.cpp
+++ src/Lab1/struct/Main.cpp
@@ -3,18 +3,19 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     Price product1, product2, product3;
-    product1 = fillData("Cheese", "Market1", 230);
-    product2 = fillData("Apple", "Market2", 500);
-    product3 = fillData( "Cherry", "Market1", 150);
+    product1.fillData("Cheese", "Market1", 230);
+    product2.fillData("Apple", "Market2", 500);
+    product3.fillData( "Cherry", "Market1", 150);
 
-    print(product1);
-    print(product2);
-    print(product3);
+    product1.print();
+    product2.print();
+    product3.print();
 
-    cout << product1.nameOfProduct << " and " << product2.nameOfProduct << Compare(product1, product2) << endl;
-    cout << product1.nameOfProduct << " and " << product3.nameOfProduct << Compare(product1, product3) << endl;
-    cout << product2.nameOfProduct << " and " << product3.nameOfProduct << Compare(product2, product3) << endl;
+    cout << product1.nameOfProduct << " and " << product2.nameOfProduct; product1.Compare(product2);
+    cout << product1.nameOfProduct << " and " << product3.nameOfProduct; product1.Compare(product3);
+    cout << product2.nameOfProduct << " and " << product3.nameOfProduct; product2.Compare(product3);
     return 0;
 }
