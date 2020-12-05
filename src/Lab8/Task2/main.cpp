fc04d79f1ea4759f65f634d594e9dcc3cdf362b7
Spirit Winner <spirit.no.win@gmail.com>
2020-12-05 14:13:32 +0300

    add task2 lab8

--- src/Lab8/Task2/main.cpp
+++ src/Lab8/Task2/main.cpp
@@ -1,4 +1,27 @@
-//
-// Created by Spirit Winner on 04.12.2020.
-//
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+void function(double x)
+{
+    if ((x <= 0) || (x == M_PI))
+    {
+        throw x;
+    }
+    double y = sqrt(pow(x, 3))/ tan(x);
+    cout << "Answer: " << y << endl;
+}
+
+int main()
+{
+    try
+    {
+        function(3.23);
+        function(M_PI);
+    }
+    catch (const double ex)
+    {
+        cerr << "Your x = " << ex << ", not include RAV" << endl;;
+    }
+}
